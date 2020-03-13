7deefb604cb7c16bea0b10855bd15c5c3610ba61
Michał Sibik <48072394+eintorwdo@users.noreply.github.com>
2020-03-13 04:27:01 +0100

    need to handle blank page load in the future

--- scraper.js
+++ scraper.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
+const fs = require('fs');
 
 const args = process.argv.slice(2);
 
@@ -17,33 +18,53 @@ for(let i=0;i<args.length;i++){
     }
 }
 
-async function scrape(url, num){
-    let tempArr = [];
-    try {
-        const response = await axios.get(url);
-        const data = response.data;
-        const $ = cheerio.load(data);
-        $('.timeline .dir-ltr').each((i, el) => {
-            let tweetHtml = $.html(el);
-            if(!$(`${tweetHtml}`).is('a') && counter < num){
-                tempArr.push($(`${tweetHtml}`).text());
+function parseBatch($, num){
+    $('.timeline .tweet').each((i, el) => {
+        // let tempArr = [];
+        let tweetHtml = $.html(el);
+        let test = tweetHtml.search(/<span class="context">[^]* retweeted<\/span>/);
+        if(test === -1){
+            let $$ = cheerio.load(tweetHtml);
+            let tweetCore = $$('.dir-ltr');
+            let coreHtml = $$.html(tweetCore);
+            if(counter < num){
+                let text = $$(`${coreHtml}`).not('a').text();
+                text = text.replace(/\n/g, ' ');
+                text = text.replace(/\\/, '');
+                text = text.replace(/"/g, "'");
+                tweetArr.push(text.trim());
                 counter++;
+                console.log(`Tweets parsed: ${counter}/${num}`);
             }
-        });
-        let moreTweetsURL = `https://mobile.twitter.com${$('.w-button-more a').attr('href')}`;
-        return [tempArr, moreTweetsURL];
-      } catch (error) {
-        console.log(error);
-      }
+        }
+    });
+}
+
+function scrape(url, num){
+    return new Promise(async (resolve, reject) => {
+        try {
+            const response = await axios.get(url, {headers: {'User-Agent': 'Mozilla/5.0 (Linux; Android 4.1; Nexus 7 Build/JRN84D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Safari/535.19'}});
+            const data = response.data;
+            const $ = cheerio.load(data);
+            setTimeout(() => {
+                parseBatch($, num);
+                let moreTweetsURL = `https://mobile.twitter.com${$('.w-button-more a').attr('href')}`;
+                console.log(moreTweetsURL);
+                resolve(moreTweetsURL);
+            }, 2000);
+        } catch (error) {
+            reject(error)
+        }
+    });
 }
 
 async function getTweets(number = 10, name = "CNN"){
     let url = `https://mobile.twitter.com/${name}`;
     while(counter < number){
-        let res = await scrape(url, number);
-        tweetArr.push(...res[0]);
-        url = res[1];
+        var res = await scrape(url, number);
+        url = res;
     }
+    fs.writeFileSync('./tweets.json', JSON.stringify([tweetArr, res]));
     console.log(tweetArr)
 }
 
