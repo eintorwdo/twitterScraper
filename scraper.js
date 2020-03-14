039e157ce112d05bd44eab4f9054a4ddcdbed340
eintorwdo <michs206@gmail.com>
2020-03-14 02:21:10 +0100

    fixed error when blank page loads (no timeline); added append option

--- scraper.js
+++ scraper.js
@@ -4,10 +4,11 @@ const fs = require('fs');
 
 const args = process.argv.slice(2);
 
-let accName, numOfTweets, counter;
+let accName, numOfTweets, counter, append, filename;
 let tweetArr = [];
 counter = 0;
-
+append = false;
+filename = "./tweets.json"
 
 for(let i=0;i<args.length;i++){
     if(args[i] === "-l"){
@@ -16,28 +17,36 @@ for(let i=0;i<args.length;i++){
     if(args[i] === "-n"){
         accName = args[i+1];
     }
+    if(args[i] === "-a"){
+        append = true;
+        if(args[i+1] && args[i+1].length > 0){
+            filename = args[i+1];
+        }
+    }
 }
 
 function parseBatch($, num){
-    $('.timeline .tweet').each((i, el) => {
-        // let tempArr = [];
-        let tweetHtml = $.html(el);
-        let test = tweetHtml.search(/<span class="context">[^]* retweeted<\/span>/);
-        if(test === -1){
-            let $$ = cheerio.load(tweetHtml);
-            let tweetCore = $$('.dir-ltr');
-            let coreHtml = $$.html(tweetCore);
-            if(counter < num){
-                let text = $$(`${coreHtml}`).not('a').text();
-                text = text.replace(/\n/g, ' ');
-                text = text.replace(/\\/, '');
-                text = text.replace(/"/g, "'");
-                tweetArr.push(text.trim());
-                counter++;
-                console.log(`Tweets parsed: ${counter}/${num}`);
+    if($('body').has('.timeline').length){
+        $('.timeline .tweet').each((i, el) => {
+            // let tempArr = [];
+            let tweetHtml = $.html(el);
+            let test = tweetHtml.search(/<span class="context">[^]* retweeted<\/span>/);
+            if(test === -1){
+                let $$ = cheerio.load(tweetHtml);
+                let tweetCore = $$('.dir-ltr');
+                let coreHtml = $$.html(tweetCore);
+                if(counter < num){
+                    let text = $$(`${coreHtml}`).not('a').text();
+                    text = text.replace(/\n/g, ' ');
+                    text = text.replace(/\\/, '');
+                    text = text.replace(/"/g, "'");
+                    tweetArr.push(text.trim());
+                    counter++;
+                    console.log(`Tweets parsed: ${counter}/${num}`);
+                }
             }
-        }
-    });
+        });
+    }
 }
 
 function scrape(url, num){
@@ -48,7 +57,8 @@ function scrape(url, num){
             const $ = cheerio.load(data);
             setTimeout(() => {
                 parseBatch($, num);
-                let moreTweetsURL = `https://mobile.twitter.com${$('.w-button-more a').attr('href')}`;
+                let morePath = $('.w-button-more a').attr('href');
+                let moreTweetsURL = morePath ? `https://mobile.twitter.com${morePath}` : undefined;
                 console.log(moreTweetsURL);
                 resolve(moreTweetsURL);
             }, 2000);
@@ -60,12 +70,27 @@ function scrape(url, num){
 
 async function getTweets(number = 10, name = "CNN"){
     let url = `https://mobile.twitter.com/${name}`;
+    let tweets;
+    if(append){
+        let tw = fs.readFileSync(filename);
+        tweets = JSON.parse(tw);
+        url = tweets[1];
+    }
     while(counter < number){
         var res = await scrape(url, number);
-        url = res;
+        if(res){
+            url = res;
+        }
     }
-    fs.writeFileSync('./tweets.json', JSON.stringify([tweetArr, res]));
-    console.log(tweetArr)
+    if(!append){
+        fs.writeFileSync(filename, JSON.stringify([tweetArr, res]));
+        console.log(tweetArr)
+    }
+    else{
+        tweets[0].push(...tweetArr);
+        fs.writeFileSync(filename, JSON.stringify([tweets[0], res]));
+        console.log(tweets[0]);
+    }
 }
 
 getTweets(numOfTweets, accName)
