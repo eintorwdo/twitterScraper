63a6f733225b0eb41b5ad312dda7dce434d09cb3
Michał Sibik <48072394+eintorwdo@users.noreply.github.com>
2020-03-19 23:21:40 +0100

    switched to puppeteer

--- scraper.js
+++ scraper.js
@@ -1,15 +1,14 @@
-const axios = require('axios');
 const cheerio = require('cheerio');
 const fs = require('fs');
 const bigInt = require("big-integer");
 const { Map, OrderedSet } = require('immutable');
+const puppeteer = require('puppeteer');
 
 const args = process.argv.slice(2);
 
 let accName, numOfTweets, counter, append, filename;
 let tweetArr = OrderedSet();
 counter = 0;
-append = false;
 filename = "./tweets.json"
 
 for(let i=0;i<args.length;i++){
@@ -19,63 +18,97 @@ for(let i=0;i<args.length;i++){
     if(args[i] === "-n"){
         accName = args[i+1];
     }
-    if(args[i] === "-a"){
-        append = true;
-        if(args[i+1] && args[i+1].length > 0){
-            filename = args[i+1];
-        }
-    }
 }
 
-function parseBatch($, num){
-    if($('body').has('.timeline').length){
-        $('.timeline .tweet').each((i, el) => {
-            if(counter < num){
-                let tweetHtml = $.html(el);
-                let test = tweetHtml.search(/<span class="context">[^]* retweeted<\/span>/);
-                if(test === -1){
-                    let $$ = cheerio.load(tweetHtml);
-                    let tweetCore = $$('.dir-ltr');
-                    let coreHtml = $$.html(tweetCore);
-                    let tweetID = bigInt($$('.tweet-text').attr('data-id'));
-                    let IDShifted = tweetID.shiftRight(22);
-                    let offset = 1288834974657;
-                    let tstamp = IDShifted.add(offset);
-                    let date = new Date(parseInt(tstamp.toString()));
-                    let text = $$(`${coreHtml}`).not('a').text();
-                    text = text.replace(/\n/g, ' ');
-                    text = text.replace(/\\/, '');
-                    text = text.replace(/"/g, "'");
-                    let tweetObj = {
-                        text: text.trim(),
-                        date: date.toString(),
-                        id: tweetID.toString().trim()
-                    }
-                    const tweetMap = Map(tweetObj);
-                    let sizeBefore = tweetArr.size;
-                    tweetArr = tweetArr.add(tweetMap);
-                    if(tweetArr.size > sizeBefore){
-                        counter++;
-                    }
-                    console.log(`Tweets parsed: ${counter}/${num}`);
-                }
+function downloadBody(){
+    const data = document.querySelector('main');
+    return data.outerHTML;
+}
+
+function extractItems(body, num){
+    let items = [];
+    const $ = cheerio.load(body);
+    $('article[aria-haspopup="false"][role="article"]').each((i, el) => {
+        if(items.length < num){
+            let tweetHtml = $.html(el);
+            if(tweetHtml.search(/role="blockquote"/) == -1 && tweetHtml.search(/<svg viewBox="0 0 0 24" aria-label/) == -1){
+                let $$ = cheerio.load(tweetHtml);
+                // let text = $$(`${tweetHtml}`).text();
+                let text = $$('div[lang][dir="auto"]').text();
+                console.log(text);
+                console.log('------------')
+                items.push(text);
             }
-        });
+        }
+    });
+    return items;
+}
+
+async function infiniteScroll(page, num){
+    let items = [];
+    let body;
+    try{
+        let previousHeight;
+        while (items.length < num) {
+            body = await page.evaluate(downloadBody);
+            items = await extractItems(body, num);
+            previousHeight = await page.evaluate('document.body.scrollHeight');
+            await page.evaluate('window.scrollTo(0, document.body.scrollHeight)');
+            await page.waitForFunction(`document.body.scrollHeight > ${previousHeight}`);
+            await page.waitFor(1000);
+        }
+        return items;
     }
+    catch(e){}
+    
+    // if($('body').has('.timeline').length){
+    //     $('.timeline .tweet').each((i, el) => {
+    //         if(counter < num){
+    //             let tweetHtml = $.html(el);
+    //             let test = tweetHtml.search(/<span class="context">[^]* retweeted<\/span>/);
+    //             if(test === -1){
+    //                 let $$ = cheerio.load(tweetHtml);
+    //                 let tweetCore = $$('.dir-ltr');
+    //                 let coreHtml = $$.html(tweetCore);
+    //                 let tweetID = bigInt($$('.tweet-text').attr('data-id'));
+    //                 let IDShifted = tweetID.shiftRight(22);
+    //                 let offset = 1288834974657;
+    //                 let tstamp = IDShifted.add(offset);
+    //                 let date = new Date(parseInt(tstamp.toString()));
+    //                 let text = $$(`${coreHtml}`).not('a').text();
+    //                 text = text.replace(/\n/g, ' ');
+    //                 text = text.replace(/\\/, '');
+    //                 text = text.replace(/"/g, "'");
+    //                 let tweetObj = {
+    //                     text: text.trim(),
+    //                     date: date.toString(),
+    //                     id: tweetID.toString().trim()
+    //                 }
+    //                 const tweetMap = Map(tweetObj);
+    //                 let sizeBefore = tweetArr.size;
+    //                 tweetArr = tweetArr.add(tweetMap);
+    //                 if(tweetArr.size > sizeBefore){
+    //                     counter++;
+    //                 }
+    //                 console.log(`Tweets parsed: ${counter}/${num}`);
+    //             }
+    //         }
+    //     });
+    // }
 }
 
 function scrape(url, num){
     return new Promise(async (resolve, reject) => {
         try {
-            const response = await axios.get(url, {headers: {'User-Agent': 'Mozilla/5.0 (Linux; Android 4.1; Nexus 7 Build/JRN84D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Safari/535.19'}});
-            const data = response.data;
-            const $ = cheerio.load(data);
-            setTimeout(() => {
-                parseBatch($, num);
-                let morePath = $('.w-button-more a').attr('href');
-                let moreTweetsURL = morePath ? `https://mobile.twitter.com${morePath}` : undefined;
-                resolve(moreTweetsURL);
-            }, 1000);
+            const browser = await puppeteer.launch({headless: true});
+            const page = await browser.newPage();
+            page.setViewport({ width: 1280, height: 926 });
+            await page.goto(url);
+            await page.waitFor('main');
+            await page.waitFor(3000);
+            let data = await infiniteScroll(page, num);
+            browser.close();
+            resolve(data);
         } catch (error) {
             reject(error)
         }
@@ -83,37 +116,10 @@ function scrape(url, num){
 }
 
 async function getTweets(number = 10, name = "CNN"){
-    let url = `https://mobile.twitter.com/${name}`;
-    let tweets;
-    if(append){
-        let tw = fs.readFileSync(filename);
-        tweets = JSON.parse(tw);
-        let lastTweetID = tweets[0][tweets[0].length - 1].id;
-        url = `https://mobile.twitter.com/${name}?max_id=${lastTweetID}`;
-        for(tweet of tweets[0]){
-            const map = Map(tweet);
-            tweetArr = tweetArr.add(map);
-        }
-    }
-    let noTweetsCounter = 0;
-    while(counter < number){
-        if(noTweetsCounter <= 4){
-            var res = await scrape(url, number);
-            if(res){
-                url = res;
-                noTweetsCounter = 0;
-            }
-            else{
-                noTweetsCounter++;
-            }
-            console.log(url);
-        }
-        else{
-            console.log('No more tweets found');
-            break;
-        }
-    }
-    fs.writeFileSync(filename, JSON.stringify([[...tweetArr], name]));
+    let url = `https://twitter.com/search?q=from%3A${name}&src=typed_query&f=live`;
+    let data = [];
+    data = await scrape(url, number);
+    fs.writeFileSync(filename, JSON.stringify([data, name]));
 }
 
 getTweets(numOfTweets, accName)
