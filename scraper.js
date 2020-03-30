60a1ac8c1e954f6f238a7411c9c79ea6a7476601
Michał Sibik <48072394+eintorwdo@users.noreply.github.com>
2020-03-30 14:12:06 +0200

    added limit of retrying attempts after reaching a limit; replies are now excluded by default

--- scraper.js
+++ scraper.js
@@ -6,10 +6,10 @@ const puppeteer = require('puppeteer');
 
 const args = process.argv.slice(2);
 
-let accName, numOfTweets, counter, tweets, filename;
-// let tweetArr = OrderedSet();
+let accName, numOfTweets, counter, filename, noReplies;
+noReplies = true;
 counter = 0;
-filename = "./tweets.json"
+filename = "./tweets.json";
 
 for(let i=0;i<args.length;i++){
     if(args[i] === "-l"){
@@ -39,7 +39,7 @@ function extractItems(body, name){
     const $ = cheerio.load(body);
     $('article[aria-haspopup="false"][role="article"]').each(async (i, el) => {
         let tweetHtml = $.html(el);
-        if(tweetHtml.search(/role="blockquote"/) == -1 && tweetHtml.search(/<svg viewBox="0 0 0 24" aria-label/) == -1){
+        if(tweetHtml.search(/role="blockquote"/) == -1){    //not a quote
             let $$ = cheerio.load(tweetHtml);
             let text = $$('div[lang][dir="auto"]').text();
             let href = $$(`a[title][href^="/${name}/status/"]`).attr('href');
@@ -65,7 +65,7 @@ async function infiniteScroll(page, num, name){
             body = await page.evaluate(downloadBody);
             let itemsArr = await extractItems(body, name);
             items = items.union(itemsArr);
-            console.log(items.size, 'of', num);
+            console.log(`$Got ${items.size} of ${num} remaining tweets`);
             previousHeight = await page.evaluate('document.body.scrollHeight');
             await page.evaluate('window.scrollTo(0, document.body.scrollHeight)');
             await page.waitForFunction(`document.body.scrollHeight > ${previousHeight}`, {timeout: 3000});
@@ -84,7 +84,7 @@ async function infiniteScroll(page, num, name){
 function scrape(url, num, name){
     return new Promise(async (resolve, reject) => {
         try {
-            const browser = await puppeteer.launch({headless: false});
+            const browser = await puppeteer.launch({headless: true});
             const context = await browser.createIncognitoBrowserContext();
             const page = await context.newPage();
             page.setViewport({ width: 1280, height: 926 });
@@ -110,10 +110,16 @@ function scrape(url, num, name){
 
 async function getTweets(number = 10, name = "CNN"){
     let dateString = '';
-    let url = `https://twitter.com/search?q=from%3A${name}${dateString}&src=typed_query&f=live`;
+    let noRepliesString = '%20-filter%3Areplies';
+    let noRetweetsString = '%20-filter%3Anativeretweets';
+    let url = `https://twitter.com/search?q=from%3A${name}${dateString}${noRetweetsString}&src=typed_query&f=live`;
+    if(noReplies){
+        url = `https://twitter.com/search?q=from%3A${name}${dateString}${noRepliesString}${noRetweetsString}&src=typed_query&f=live`;
+    }
     let data;
     data = await scrape(url, number, name);
     let remainingTweets = number - data.size;
+    let limitCounter = 0;
     while(data.size < number){
         let dataArray = data.toArray();
         let lastTweet = dataArray[dataArray.length - 1].toJS();
@@ -124,8 +130,10 @@ async function getTweets(number = 10, name = "CNN"){
         let day = date.getDate();
         dateString = `${year}-${('0'+(month+1)).slice(-2)}-${('0'+day).slice(-2)}`;
         dateString = `%20until%3A${dateString}`;
-        console.log('date string: ',dateString);
-        url = `https://twitter.com/search?q=from%3A${name}${dateString}&src=typed_query&f=live`;
+        url = `https://twitter.com/search?q=from%3A${name}${dateString}${noRetweetsString}&src=typed_query&f=live`;
+        if(noReplies){
+            url = `https://twitter.com/search?q=from%3A${name}${dateString}${noRepliesString}${noRetweetsString}&src=typed_query&f=live`;
+        }
         let newData = await scrape(url, remainingTweets, name);
         let sizeBefore = data.size;
         data = data.union(newData);
@@ -133,13 +141,25 @@ async function getTweets(number = 10, name = "CNN"){
         remainingTweets = number - data.size;
         if(sizeBefore == sizeAfter){
             remainingTweets += 200; //to avoid looping over the same tweets
+            limitCounter++;
+        }
+        else{
+            limitCounter = 0;
+        }
+        if(limitCounter > 2){
+            console.log('Could not find more tweets, saving...');
+            break;
         }
     }
     data = data.toArray();
     let numToDelete = data.length - number;
-    for(let i=0;i<numToDelete;i++){
-        data.pop();
+    if(numToDelete > 0){
+        for(let i=0;i<numToDelete;i++){
+            data.pop();
+        }
     }
+    console.log(`Tweets scraped: ${data.length}`);
+    console.log('Saving...');
     fs.writeFileSync(filename, JSON.stringify([data, name]));
 }
 
